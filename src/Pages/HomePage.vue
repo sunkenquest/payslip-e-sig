29f6fd16ed3ab1892a9f16857abe647579a7ab27
sunkenquest <sunkenquest@gmail.com>
2024-09-19 11:49:41 +0800

    chore: atomic file upload

--- src/Pages/HomePage.vue
+++ src/Pages/HomePage.vue
@@ -2,6 +2,11 @@
   <div class="main-container">
     <h1 class="font-bold text-lg mx-auto md:mx-0">Sign Payslip</h1>
   </div>
+  <FileUploadOrganism class="mx-auto my-auto" />
+  <ImageUploadOrganism class="mx-auto my-auto" />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ImageUploadOrganism from '../components/Organisms/ImageUploadOrganism.vue';
+import FileUploadOrganism from '../components/Organisms/FileUploadOrganism.vue';
+</script>
---
+++ src/components/Organisms/FileUploadOrganism.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="main-container flex flex-col justify-center items-center space-y-5">
+    <FileInput @change="handleFileChange" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import FileInput from '../Molecules/FileInputMolecule.vue';
+
+const handleFileChange = (file: File) => {
+  console.log('File selected:', file);
+};
+</script>
