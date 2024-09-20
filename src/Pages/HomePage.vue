c4eaa8341afadd15093052de13452fe33f5c0cdc
emmanueljustin <ejatienza01@gmail.com>
2024-09-20 14:10:17 +0800

    chore: minor adjustments for css class

--- src/Pages/HomePage.vue
+++ src/Pages/HomePage.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="flex flex-col justify-center items-center min-h-screen">
+  <div class="flex flex-col justify-center items-center min-h-scree">
     <TitleMolecule />
-    <div class="flex flex-col md:flex-row w-full md:w-[60%] items-center space-y-5 md:space-y-4 md:space-x-5">
+    <div class="flex md:flex-row w-full md:w-[60%] items-center space-y-5 md:space-y-4 md:gap-5">
       <FileUploadOrganism class="w-full flex-2" />
       <ImageUploadOrganism class="w-full flex-1" />
     </div>
-    <div>
+    <div class="w-[60%] mt-5">
       <input @click="pdfSignStore.SignPdf"
-        class="font-bold text-white bg-blue-700 rounded-md py-2 px-5 cursor-pointer active:bg-blue-400" type="button"
-        value="Sign My PDF" />
+        class="w-[100%] font-bold text-white bg-blue-700 rounded-md py-2 px-5 cursor-pointer active:bg-blue-400"
+        type="button" value="Sign My PDF" />
       <iframe v-if="pdfSignStore.signedPdf" :src="pdfSignStore.signedPdf.toString()" width="600" height="500"
         frameborder="0"></iframe>
     </div>
 
-    <div v-if="pdfSignStore.status == EventStatus.failed">
+    <div v-if="pdfSignStore.status === EventStatus.failed">
       <h1 class="font-bold text-red-900">{{ pdfSignStore.error }}</h1>
     </div>
   </div>
@@ -24,9 +24,9 @@ import ImageUploadOrganism from '../components/Organisms/ImageUploadOrganism.vue
 import FileUploadOrganism from '../components/Organisms/FileUploadOrganism.vue';
 import TitleMolecule from '../components/Molecules/TitleMolecule.vue';
 import { usePdfSignStore } from '../stores/pdf_sign';
-import { useSignatureStore } from '../stores/signature';
+// import { useSignatureStore } from '../stores/signature';
 import { EventStatus } from '../types/enums/status';
 
 const pdfSignStore = usePdfSignStore();
-const signaturestore = useSignatureStore();
+// const signaturestore = useSignatureStore();
 </script>
--- src/components/Atoms/ButtonAtom.vue
+++ src/components/Atoms/ButtonAtom.vue
@@ -1,10 +1,6 @@
 <template>
-  <button
-    @click="$emit('click')"
-    type="button"
-    :class="buttonClasses"
-    class="text-white focus:outline-none focus:ring-4 font-medium rounded-full text-sm px-5 py-2.5 text-center"
-  >
+  <button @click="$emit('click')" type="button" :class="buttonClasses"
+    class="text-white focus:outline-none focus:ring-4 font-medium rounded-full text-sm px-5 py-2.5 text-center">
     {{ label }}
   </button>
 </template>
@@ -25,11 +21,11 @@ const props = defineProps({
 
 const buttonClasses = computed(() => {
   const colorMap: Record<string, string> = {
-    blue: 'bg-blue-700 hover:bg-blue-800 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800',
-    red: 'bg-red-700 hover:bg-red-800 focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800',
-    green: 'bg-green-700 hover:bg-green-800 focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800',
+    blue: 'button-color-blue',
+    red: 'button-color-red',
+    green: 'button-color-green',
   };
-  
+
   return colorMap[props.color] || colorMap.blue;
 });
 </script>
